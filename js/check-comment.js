dd926cf8819cde8403275eddebcf1117acf02d4a
Thomas <thomas91@list.ru>
2018-08-29 15:12:10 +0300

    Comment

--- js/check-comment.js
+++ js/check-comment.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
+	(function(){
 
-	console.log('Hello world!');
+		var checkComment = {
+			init: function(){
+				this._setupListner();
+			},
+
+			_setupListner: function(){
+				$('#comment-form').on('submit', checkComment._validateForm)
+			},
+
+			_validateForm: function(event){
+				event.preventDefault();
+				var form = $(this),
+						textarea = form.find('.textarea');
+						textParent = textarea.parents();
+				console.log(textParent);
+				if (textarea.val().trim() == ""){
+					console.log("В условии");
+					event.preventDefault();
+					textParent.find('.notify').removeClass('hide');
+				} else {
+					textParent.find('.notify').addClass('hide');
+					$('#comment-form').unbind('submit').submit();
+				}
+			},
+		};
+		checkComment.init();
+
+	}());
+
 
 });
